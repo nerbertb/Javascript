2800355291416cfa9a819336fcd864c71e71547f
Nerbs <nerbertb@gmail.com>
2025-05-15 02:02:14 +0800

    fix: updated while loop
    Updated while loop and fix the last part of the song when the bottle becomes 0.

--- 99 Bottles - While/99 Bottles Exercise/script.js
+++ 99 Bottles - While/99 Bottles Exercise/script.js
@@ -1,11 +1,27 @@
 numBottles = 99;
 
 
-while (numBottles > 0) {
+while (numBottles > 1) {
 
-    console.log(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer. Take one down and pass it around, " + (numBottles - 1) + " bottles of beer on the wall.");
+    
+    console.log(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer.\nTake one down and pass it around, " + (numBottles - 1) + " bottles of beer on the wall.");
 
     numBottles--;
+
+    if (numBottles === 1) {
+        
+        console.log(numBottles + " bottle of beer on the wall, " + numBottles + " bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.");
+
+    numBottles--;
+
+        if (numBottles === 0){
+        console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+    } 
+    
+
+
+    
 }
 
 
