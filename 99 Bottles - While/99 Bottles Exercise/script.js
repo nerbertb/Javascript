26ffec06b40467a462018cacde0c8d2bd6791cbb
Nerbs <nerbertb@gmail.com>
2025-05-15 01:32:43 +0800

    feat: 99 Bottles exercise
    Simple usage of while loop.

---
+++ 99 Bottles - While/99 Bottles Exercise/script.js
@@ -0,0 +1,12 @@
+numBottles = 99;
+
+
+while (numBottles > 0) {
+
+    console.log(numBottles + " bottles of beer on the wall, " + numBottles + " bottles of beer. Take one down and pass it around, " + (numBottles - 1) + " bottles of beer on the wall.");
+
+    numBottles--;
+}
+
+
+
