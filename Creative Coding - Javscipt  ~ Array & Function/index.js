ee9844c109f2cfba49a9d76d54ea5ab7a8cdd0de
Nerbs <nerbertb@gmail.com>
2025-05-23 23:50:42 +0800

    feat: Random Boxes Generator
    Learned about function, arrays, and if statement and was able to create a boxes that generates random boxes inside of it.

--- Creative Coding - Javscipt  ~ Array & Function/index.js
+++ Creative Coding - Javscipt  ~ Array & Function/index.js
@@ -1,47 +1,49 @@
 
 
+
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
 context.fillStyle = 'blue';
+// context.fillRect(100,100,400,400);
 
-
-context.lineWidth = 4;
-// context.beginPath();
-
-
-// context.arc(300, 300, 200, 0, Math.PI*2);
-// context.stroke();
-
-
-// context.fillRect    (100, 100, 400, 400);
 
 context.beginPath();
-context.rect(100, 100, 400, 400);
-// context.stroke();
+context.lineWidth = 4;
+// context.rect(100,100,400,400);
+context.stroke();
 
 
+context.beginPath();
+// context.arc(300,300,100, 0, Math.PI * 2);
+context.stroke();
 
 
-for (let i = 0;i < 5; i++) {
+for (let i = 0; i < 5; i++){
     
-    for (let j = 0;j < 5; j++) {
         
-        let width = 40;
-        let height = 40;
-        let gap = 20;
-        let x = 100 + (width + gap)  * i;
-        let y = 100 + (height + gap) * j;
+        for (let j = 0; j < 5; j++) {
+             
+            let width = 60;
+    let height = 60;
+    let gap = 20;
+    let x = 100 + (width + gap) * i;
+    let y = 100 + (height + gap) * j;
+        
+
+            context.beginPath();
+            context.rect(x, y, width, height);
+            context.stroke();
+            
+            if (Math.random() > 0.5 ) {
+
+                context.beginPath();
+                context.rect(x + 8, y + 8, width -   16, height - 16)
+                context.stroke();
+            }
 
 
-        context.beginPath();
-        context.rect(x, y, width, height);
-        context.stroke();
+    }
 
-        context.beginPath();
-        context.rect(x + 8,y + 8,width - 16,height - 16)
-        context.stroke();
-     }
-    
 
 }
